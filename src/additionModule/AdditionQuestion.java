08313fb98def227e282d451c4a9625c19a21b96e
Mike Watson <watsonmf@gmail.com>
2016-04-12 00:35:12 -0600

    Rename MathQuestion.java to AdditionQuestion.java

---
+++ src/additionModule/AdditionQuestion.java
@@ -0,0 +1,23 @@
+package additionModule;
+
+import tenjinMath.Question;
+
+public class AdditionQuestion extends tenjinMath.Question
+{
+	private int[] possibleAnswers;
+	private int[] operands;
+	private char[] operators;
+	
+	public AdditionQuestion()
+	{
+		
+	}
+	
+	@Override
+	public boolean checkAnswer(String answer)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
