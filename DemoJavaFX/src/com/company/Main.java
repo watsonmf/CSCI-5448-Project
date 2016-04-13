1b70fcab0e1c387815309b84db1b4c8d5ab42515
Erik Eakins <erik.eakins@colorado.edu>
2016-04-13 16:56:57 -0600

    Trying to make check boxes exclusive

--- DemoJavaFX/src/com/company/Main.java
+++ DemoJavaFX/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
